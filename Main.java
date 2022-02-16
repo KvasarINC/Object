32bb095468cc3a655b515119b9fcd83f45fadd43
KvasarINC <99033916+KvasarINC@users.noreply.github.com>
2022-02-16 15:02:05 +0200

    Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.learning;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Robots Bender = new Robots();
+      //  Robots Sam = new Robots();
+
+        Bender.name = "Bender";
+        Bender.hight = 178.5f;
+        Bender.width = 54.5f;
+
+
+        Bender.PrintParament();
+   //     Sam.your_name();
+    }
+
+}
