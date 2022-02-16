89814eb9e7d5e1060c04f759ba7322bbf58a8d7a
KvasarINC <99033916+KvasarINC@users.noreply.github.com>
2022-02-16 15:02:40 +0200

    Robots.java

---
+++ Robots.java
@@ -0,0 +1,36 @@
+package com.learning;
+
+import java.util.Scanner;
+
+public class Robots {
+    String name;
+    float hight;
+    float width;
+
+    String your_name;
+    int your_width;
+    int your_hight;
+
+
+    public void PrintParament() {
+        System.out.println("My name is: " + name);
+        System.out.println("My height: " + hight);
+        System.out.println("My width " + width);
+
+        System.out.println("Please enter your robot's name");
+    }
+
+
+
+//    public void YourPrint() {
+
+  //      System.out.println("My name is: " + your_name);
+      //  System.out.println("My height: " + your_hight);
+    //    System.out.println("My width " + your_width);
+   // }
+
+   // public void your_name() {
+   //     Scanner scanner = new Scanner(System.in);
+     //   your_name = scanner.nextLine();
+   // }
+}
